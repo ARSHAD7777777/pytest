056b15deb63954f5cbf216ddf60dc2cffed8f5cc
ARSHAD KADEVALAPPIL <96134681+ARSHAD7777777@users.noreply.github.com>
2023-10-08 13:03:42 +0200

    Update test_seq_func.py

--- test_seq_func.py
+++ test_seq_func.py
@@ -3,10 +3,8 @@
 
 # In[ ]:
 
-import numpy as np
-import seq_func
+
 def test_pattern_rec():
-        array=np.array[[1,2,4],[5,6,7]]
-        obj=seq_func.pattern_rec(array)
-        assert obj=='rectangle'
+        obj=seq_func.pattern_rec(2,3)
+        assert obj==5
 
