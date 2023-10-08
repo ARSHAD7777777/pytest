f243113d2da244313af4bce33ab40dd19e01ec8b
ARSHAD KADEVALAPPIL <96134681+ARSHAD7777777@users.noreply.github.com>
2023-10-08 13:02:20 +0200

    Update seq_func.py

--- seq_func.py
+++ seq_func.py
@@ -4,10 +4,7 @@
 # In[1]:
 
 
-def pattern_rec(array):
-    if array.shape[0]==array.shape[1]:
-        obj='square'
-    else:
-        obj='reactangle'
-    return obj
+def pattern_rec(a,b):
+    return (a+b)
+    
 
